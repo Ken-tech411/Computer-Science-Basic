b0249e1885242a4e721af9e6b7a5928f607a33aa
Ken-tech411 <71333435+Ken-tech411@users.noreply.github.com>
2021-10-23 21:06:33 +0700

    end 23-10-2021

--- 23-10-2021/index.py
+++ 23-10-2021/index.py
@@ -1,3 +1,4 @@
+import math
 # Bai 1
 # num1 = int(input("Put a number: "))
 # num2 = int(input("Put a number: "))
@@ -30,11 +31,71 @@
 #     print("You are obese!")
 
 # Bai 3
-a = int(input("Put a number: "))
-b = int(input("Put a number: "))
-c = int(input("Put a number: "))
 
-if a + b > c and b + c > a and a + c > b:
-    print("It can be a triangle!")
-else: 
-    print("It cannot be a triangle!")
+# a = int(input("Put a number: "))
+# b = int(input("Put a number: "))
+# c = int(input("Put a number: "))
+
+# if a + b > c and b + c > a and a + c > b:
+#     print("It can be a triangle!")
+# else: 
+#     print("It cannot be a triangle!")
+
+# Bai 4
+# a = float(input("Input a: "))
+# b = float(input("Input b: "))
+# c = float(input("Input c: "))
+
+# delta = delta = b**2 - 4 * a * c
+# if delta > 0:
+#     x1 = (-b + math.sqrt(delta)) / (2 * a)
+#     x2 = (-b - math.sqrt(delta)) / (2 * a)
+#     print(x1, x2)
+# elif a == 0:
+#     x1 = (-b) / (2 * a)
+#     x2 = x1
+#     print(x1, x2)
+# else:
+#     print("Đổi số đi bạn êy =))))))))")
+
+# Bai 5 
+# year = float(input("Put the year: "))
+# sodu = [0, 3, 6, 9, 11, 14, 17]
+# chia = year % 19
+
+# if year % 4 == 0:
+#     print("This is leap year")
+# elif (chia in sodu):
+#     print("This is leap lunar year")
+# else: 
+#     print("This is not leap year")
+
+# Bai 6
+# practice = float(input("Put your score: "))
+# hw_theory = float(input("Put your score: "))
+# theory = float(input("Put your score: "))
+
+# total = (0.30 * practice) + (0.30 * hw_theory) + (0.40 * theory)
+
+# if (total >= 5):
+#     print("Congrats, you passed")
+# else:
+#     print("Congrats, you failed. Study again =))))")
+
+# Bai 7 
+average = float(input("Input: "))
+
+if average >= 9.0 :
+    print("Output: Outstanding")
+elif 8.0 <= average <= 8.99 :
+    print("Output: Excellent")
+elif 7.0 <= average <= 7.99 :
+    print("Output: Good")
+elif 6.0 <= average <= 6.99 :
+    print("Output: Normal")
+elif 5.0 <= average <= 5.99 :
+    print("Output: Average")
+elif 4.0 <= average <= 4.99 :
+    print("Output: Poor")
+elif average <= 4:
+    print("Output: Thằng gà mờ =)))))))")
