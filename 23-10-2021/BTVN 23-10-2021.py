d1311eb2afe7cd454db68f86542ee4d522582d3e
Ken-tech411 <71333435+Ken-tech411@users.noreply.github.com>
2021-10-24 20:32:26 +0700

    BTVN 23-10-2021

---
+++ 23-10-2021/BTVN 23-10-2021.py
@@ -0,0 +1,26 @@
+# Bai 1 
+# number = int((input("Input number: ")))
+
+# if (number % 2 != 0):
+#     print(f"{number} is an odd number")
+# else:
+#     print(f"{number} is an even number")
+
+# Bai 2
+num = float((input("Input number: ")))
+
+if (num % 1 != 0):
+    print(f"{num} is not an interger")
+else:
+    print(f"{num} is an interger")
+
+# Bai 3   
+# input = (input("Input character: "))
+# character = str(input)
+
+# if input != character:
+#     print(f"{input} is a digit")
+# else:
+#     print(f"{input} is not a digit")
+
+# Bai 4 
